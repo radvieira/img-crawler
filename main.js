941eab042df396167e0d9407e947475294a84a9d
Raul Vieira <radvieira@gmail.com>
2013-01-25 18:11:50 -0500

    Refactoring crawler to be a little more OO

--- main.js
+++ main.js
@@ -1,89 +1,109 @@
 var request = require('request'),
+
 	htmlparser = require("htmlparser");
 
-var getSourceWhenImage = function(element) {
-	
-	if(element.name === 'img' && element.attribs && element.attribs.src) {
-		return 	element.attribs.src.trim();
-	}
-	
-};
+var webPage = function(url, onComplete) {
 
-var collectImages = function(imageSrcs, elements) {
-	
-	var i, element, src;
-	
-	for(i = 0; i < elements.length; i++) {
+	var getSourceWhenImage = function(element) {
 		
-		element = elements[i];
-		src = getSourceWhenImage(element);
-		
-		if(src) {				
-			imageSrcs.push(src);
-		} else if(element.children) {
-			collectImages(imageSrcs, element.children);			
+		if(element.name === 'img' && element.attribs && element.attribs.src) {
+			return 	element.attribs.src.trim();
 		}
-
-	}
-	
-};
-
-var getImagesFromBody = function(body, callback) {
-
-	var handler = new htmlparser.DefaultHandler(function (error, dom) {
-
-		var imageSrcs = [];
-
-		collectImages(imageSrcs, dom);								
-
-		callback(undefined, {srcs: imageSrcs});
-
-	});
-	
-	var parser = new htmlparser.Parser(handler);
-	parser.parseComplete(body);
-
-};
-
-var getPageLoadHandler = function(callback) {
-	
-	return function(err, response, body) {
 		
-		if(!err) {
-	
-			switch(response.statusCode) {
-				
-				case 200: 
-					
-					getImagesFromBody(body, callback);
-	
-					break;
-				
-				case 404:
-	
-					callback('Page not found');
-					
-					break;
-					
-				default:
-				
-					callback('Received an unsupported response code ' + response.statusCode);
-			}
-			
-		} else {
-		
-			callback();
-		
-		}
 	};
+	
+	var collectImages = function(imageSrcs, elements) {
+		
+		var i, element, src;
+		
+		for(i = 0; i < elements.length; i++) {
+			
+			element = elements[i];
+			src = getSourceWhenImage(element);
+			
+			if(src) {				
+				imageSrcs.push(src);
+			} else if(element.children) {
+				collectImages(imageSrcs, element.children);			
+			}
+	
+		}
+		
+	};
+	
+	var getImageSourcesFromBody = function(body) {
+	
+		var handler = new htmlparser.DefaultHandler(function (error, dom) {
+	
+			var imageSrcs = [];
+	
+			collectImages(imageSrcs, dom);								
+	
+			onComplete(undefined, {srcs: imageSrcs});
+	
+		});
+		
+		new htmlparser.Parser(handler).parseComplete(body);
+	
+	};
+	
+	var handlePageResponse = function(response, body) {
+
+		switch(response.statusCode) {
+			
+			case 200: 
+				
+				getImageSourcesFromBody(body);
+				break;
+			
+			case 404:
+	
+				onComplete('Page not found');
+				break;
+				
+			default:
+			
+				onComplete('Received an unsupported response code ' + response.statusCode);
+		}
+		
+	};
+	
+	var onPageLoaded = function() {
+		
+		return function(err, response, body) {
+			
+			if(!err) {
+			
+				handlePageResponse(response, body);
+				
+			} else {
+			
+				onComplete();
+			
+			}
+		};
+	};
+	
+	var loadWebPage = function() {
+	
+		request(url, onPageLoaded());		
+	
+	};
+
+	return {
+	
+		crawl: function(callback) {
+			
+			loadWebPage(callback);
+			
+		}
+	
+	};
+
 };
 
 module.exports.crawl = function(url, callback){
-	
-	request(url, getPageLoadHandler(callback));
+
+	webPage(url, callback).crawl();
 
 };
-	
-
-
-	
