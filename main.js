67b1222534b470a53789a532024cbd79a8faf564
Raul Vieira <radvieira@gmail.com>
2013-02-05 22:37:40 -0500

    Decided to move the img into its own module (again) to simplify code

--- main.js
+++ main.js
@@ -1,61 +1,8 @@
 var request = require('request'),
 
-	fs = require('fs'),
-
-	htmlparser = require("htmlparser");
-
-var img = function(host, imgPath){
-
-	var url;
+	htmlparser = require('htmlparser'),
 	
-	if(imgPath) {
-		
-		url = require('url').parse(host);
-		
-		return {
-			
-			write: function(dir, onComplete) {
-			
-				var uri = url.protocol + '//' + url.hostname + ':' + url.port + '/' + imgPath;
-					
-				var writeTo = dir + '/' + imgPath.replace('/', '-');
-				
-				var writeToDisk = function() {
-					
-					var stream = fs.createWriteStream(writeTo);
-
-					stream.on('close', function(){
-						onComplete({
-							src: imgPath,
-							path: writeTo
-						});							
-					});
-					
-					request(uri).on('response', function(response) {
-						if(response.statusCode === 200) {
-							this.pipe(stream);
-						}
-					});
-					
-				};
-			
-				fs.exists(dir, function(exists) {
-					if(!exists) {
-						fs.mkdir(dir, function() {
-							writeToDisk();
-						});
-					} else {
-						writeToDisk();
-					}
-				});
-	
-			}
-			
-		}
-		
-	}
-
-};
+	img = require('./lib/img');
 
 var webPage = function(config, onComplete) {
 	
